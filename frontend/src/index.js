07afbb025faee9526697af43fb8643ca53765a8e
Rosy_JR <silverluo24@gmail.com>
2025-07-03 16:36:50 -0400

    ready to deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,4 +21,3 @@ root.render(
         </Provider>
     </React.StrictMode>
 );
-
